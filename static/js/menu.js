afdc755d1fb1b418ae5bd74ccebc6c8807054830
chenzhiming <timczm@foxmail.com>
2024-04-28 21:10:37 +0800

    文章阅读页面的目录样式调整为折叠面板

--- static/js/menu.js
+++ static/js/menu.js
@@ -22,4 +22,27 @@
 		menuBtn.addEventListener('click', toggleMenu, false);
 		menu.addEventListener('transitionend', removeMenuTransition, false);
 	}
+
+	// Get the TOC title and menu elements
+	var tocTitle = document.querySelector('.toc__title');
+	var tocMenu = document.querySelector('.toc__menu');
+	var tocIcon = document.querySelector('.toc__icon');
+	// 获取tocIcon下的svg元素
+	var tocIconSvg = tocIcon.querySelector('svg');
+
+	// Add click event listener to the TOC title
+	tocTitle.addEventListener('click', function() {
+		tocTitle.classList.toggle('active');
+		tocMenu.classList.toggle('active');
+		tocIconSvg.classList.toggle('active');
+
+		// If menu is active, calculate its height based on content
+		if (tocMenu.classList.contains('active')) {
+			// Show the menu and set its height based on its content
+			tocMenu.style.height = tocMenu.scrollHeight + "px";
+		} else {
+			// Hide the menu by setting its height to 0
+			tocMenu.style.height = "0";
+		}
+	});
 }(document, window));
